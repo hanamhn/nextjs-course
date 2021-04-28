31bc7ca7bc5312207fba50d479b15348f159d73a
hanamhn <hanamhndev@gmail.com>
2021-04-28 10:32:56 +0700

    project time-working

---
+++ pages/events/index.js
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+import { getAllEvents } from "../../helpers/api-util";
+
+const AllEventsPage = (props) => {
+  const { events } = props;
+  const router = useRouter();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
+
+  return (
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+};
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
 
-export default function Home() {
+function HomePage(props) {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!</h1>
+    <div>
+      <EventList items={props.events} />
     </div>
-  )
+  );
 }
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+export default HomePage;
